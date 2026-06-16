6aa28fe5e73e75d2db95693e4b2206e2b2f37427
tatsuyaa <zhairel.lingasa@gmail.com>
2026-06-16 13:33:57 +0800

    fix(ebook): add missing User model import in AdminEbookController

--- app/Http/Controllers/AdminEbookController.php
+++ app/Http/Controllers/AdminEbookController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Ebook;
 use App\Models\EbookAccessLog;
+use App\Models\User;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
