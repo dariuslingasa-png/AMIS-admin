e8f28e2f6e8b10f843e449ede4dd357e447eba4f
tatsuyaa <zhairel.lingasa@gmail.com>
2026-07-23 15:50:11 +0800

    Fix Print Color ID Sheet button rendering student list instead of ID Card print view

--- app/Http/Controllers/Admin/StudentController.php
+++ app/Http/Controllers/Admin/StudentController.php
@@ -157,7 +157,7 @@ class StudentController extends Controller
             default => $query->orderBy('students.id', 'desc'),
         };
 
-        $isPrint = $request->boolean('print_info') || $request->boolean('print_credentials');
+        $isPrint = $request->boolean('print_info') || $request->boolean('print_credentials') || $request->boolean('print_id');
 
         if ($isPrint) {
             $students = $query->get();
